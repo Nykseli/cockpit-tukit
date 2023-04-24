a85c2fb85a82a168fdd75a7395312522479f1c15
Miika Alikirri <miika.alikirri@suse.com>
2023-04-24 18:30:20 +0300

    More accurate cockpit types

--- types/cockpit.d.ts
+++ types/cockpit.d.ts
@@ -2,13 +2,71 @@ import { TODO_TYPE } from "@/todo";
 
 // https://github.com/cockpit-project/cockpit/pull/13214
 
+interface Func1<T, R = void> {
+    (arg: T): R;
+}
+
+declare type DbusOptions = {
+    bus: "session" | "user" | "system" | "none";
+    address: string;
+    superuser?: "require";
+    track: boolean;
+};
+
+type Fail = {
+    message: string;
+    problem?: string;
+};
+
+type SpawnFail = Fail & {
+    exit_status?: number;
+    exit_signal?: number;
+};
+
+type ErrorConfig = 'message' | 'out' | 'ignore' | 'pty';
+type Superuser = 'require' | 'try';
+type ProblemCodes = 'access-denied' | 'authentication-failed' | 'internal-error' | 'no-cockpit' | 'no-session' | 'not-found' | 'terminated' | 'timeout' | 'unknown-hostkey' | 'no-forwarding';
+
+type SpawnConfig = {
+    err?: ErrorConfig;
+    binary?: boolean;
+    directory?: string;
+    host?: string;
+    environ?: string[];
+    pty?: boolean;
+    batch?: boolean;
+    latency?: number;
+    superuser?: Superuser;
+};
+
+interface DbusClient {
+    wait(callback?: string): Promise<TODO_TYPE>;
+    close(problem?: string): void;
+    proxy(interface?: string,path?: string): TODO_TYPE;
+    proxies(interface?: string[], path?: string[]): TODO_TYPE[];
+    options: DbusOptions;
+    unique_name: string;
+
+}
+
+interface ClosableWithProblem { close(problem?: ProblemCodes): void; }
+
+interface SpawnPromise extends Promise<string>, ClosableWithProblem {
+    stream(callback: Func1<string>): SpawnPromise;
+    input(data?: string | Uint8Array, stream?: boolean): SpawnPromise;
+}
+
+
 declare module "cockpit" {
     function gettext(text: string): string;
-    function format(text: string, ...args: unknown[]): string;
-    function dbus(...args: TODO_TYPE[]): TODO_TYPE;
-    function script(script: string, args: TODO_TYPE): string;
-    function jump(todo: string, arg1: TODO_TYPE): TODO_TYPE;
-    function spawn(todo: TODO_TYPE, arg1: TODO_TYPE): TODO_TYPE;
+    function gettext(context: string, text: string): string;
+    function format(template: string, args: string[] | Object ): string;
 
-    const transport: {host: TODO_TYPE};
+    function dbus(name: string, options?: DbusOptions[]): DbusClient;
+
+    function jump(todo: string, host?: string | null): void;
+    function script(script: string, args: SpawnConfig): SpawnPromise;
+    function spawn(args: string | string[], options?: SpawnConfig): SpawnPromise;
+
+    const transport: { host: TODO_TYPE };
 }
